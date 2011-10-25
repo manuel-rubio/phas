35d5b1be48b8443d6c6ad0a66e8f56e11690dbd1
Manuel Rubio <manuel@altenwald.com>
2011-10-25 20:52:21 +0000

    eliminada base de datos de sqlite de la ruta base y movido a /tmp

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,7 +4,7 @@ include_once(__DIR__ . "/../lib/PHAS.php");
 
 $logfile = __DIR__ . "/../phas.log";
 $database = array (
-	"DSN" => "sqlite:" . __DIR__ . "/../main.sqlite"
+	"DSN" => "sqlite:/tmp/main.sqlite"
 );
 
 $phas = new PHAS();
