5b5e48418bbbe2d98ea1c19b714bb7f38d30c850
Manuel Rubio <manuel@altenwald.com>
2011-10-26 12:20:25 +0000

    agregado soporte de expiracion para sesion a PHPSession

--- lib/PHAS/Session/PHPSession.php
+++ lib/PHAS/Session/PHPSession.php
@@ -3,11 +3,17 @@
 class PHPSession {
     private $session;
     public function __construct($session_id=null) {
+        global $session_expires;
         if ($session_id) {
             session_id($session_id);
         }
+        if ($session_expires) {
+            session_cache_expire(round($session_expires/60));
+        }
         session_start();
         $this->session =& $_SESSION;
+    }
+    public function gc() {
     }
     public function session_id() {
         return session_id();
