8af23b15ab86d6c660363cac9752f132765fa753
Manuel Rubio <manuel@altenwald.com>
2011-11-10 00:18:23 +0000

    eliminados ficheros de antiguo status

--- htdocs/js/status.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    // Accordion
-    $("#databases").accordion({ header: "h5" });
-    $("#salidas").accordion({ header: "h5" });
-    $("#funciones").accordion({ header: "h5" });
-});
